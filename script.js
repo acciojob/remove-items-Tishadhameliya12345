b8c36aa5f21791120168827efeea27fbe816d920
Tishadhameliya12345 <159407986+Tishadhameliya12345@users.noreply.github.com>
2025-02-14 09:47:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", ()=> {
+    const colorSelect = document.getElementById("colorSelect");
+    const removeButton = document.querySelector('input[type="button"]');
+
+    removeButton.addEventListener("click", ()=> {
+        const selectedOption = colorSelect.selectedIndex;
+        if (selectedOption !== -1) {
+            colorSelect.remove(selectedOption);
+        }
+    });
+});
